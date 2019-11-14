08e056635794885510c5981dfe3f2cc8a33cae9c
Kevin Grant <ConsciousK@yahoo.com>
2019-11-13 20:12:00 -0500

    App BoilerPlated. Signup Form Basic completed...WIP.

---
+++ dev-libs-ptct/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './components/form.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        This is the beginning of an app
+        <LoginForm />
+    </div>
+  );
+}
+
+export default App;
